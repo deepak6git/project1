2a107a1cff7d65bfefcab63b99730640059f9276
deepak6git <70337075+deepak6git@users.noreply.github.com>
2020-09-19 22:23:34 +0530

    project
    my project

---
+++ hostname_socket .py
@@ -0,0 +1,10 @@
+import socket
+
+hostname = socket.gethostname()
+print (hostname)
+getIpAddress = socket.gethostbyname(hostname)
+print (getIpAddress)
+if '168' in getIpAddress:
+    print ("Ip is in the static range")
+else:
+    print ("Ip is not in the static range")
